9afba6ec85a1d5a6cb134576558ddf3605a76bdc
Jordi Suijkerbuijk <j.suijkerbuijk@hotmail.com>
2019-10-06 19:58:03 +0200

    asd
    asdasd

--- music_player/src/components/musicPlayer/musicPlayer.js
+++ music_player/src/components/musicPlayer/musicPlayer.js
@@ -1,17 +1,26 @@
 import React from "react";
+import soundFile from "../../public/songs/Kendrick_Lamar-Money_Trees.mp3";
 
 import "./musicplayer.scss";
 
-function musicPlayer() {
-  dragElement(
-    document.getElementsByClassName("musicPlayerSliderTimeIndicator")
-  );
+function startAudio() {
+  var element = document.getElementsByClassName("musicPlayerPlayButton");
+  var audio = new Audio(soundFile);
+  audio.play();
 
-  function dragElement(elmnt) {
-    console.log(elmnt);
-    var pos0 = 0,
-      pos1 = 0;
-  }
+  element.className += "hide";
+}
+
+function pauseAudio() {
+  document.getElementsByClassName("musicPlayerPlayButton");
+  var audio = new Audio(soundFile);
+  audio.pause();
+}
+
+function musicPlayer() {
+  const selectedSong = "Kendrick_Lamar-Money_Trees.mp3";
+  const audioSource = "../../public/songs/" + selectedSong;
+  console.log(audioSource);
 
   return (
     <div className="musicPlayerBlock">
@@ -22,6 +31,7 @@ function musicPlayer() {
       />
 
       {/* Range Slider */}
+      {/* <div className="musicPlayerSliderPadding"> */}
       <div className="musicPlayerSlider">
         <div className="musicPlayerSliderTime">
           <p>4:30</p>
@@ -34,16 +44,32 @@ function musicPlayer() {
           id="musicPlayerSliderTimeIndicator"
         />
       </div>
+      {/* </div> */}
 
       {/* Play button */}
-      <div className="musicPlayerPlayButton">
+
+      <div className="musicPlayerPlayButton" onClick={startAudio}>
+        <audio id="audio" src={soundFile} />
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 26">
           <polygon
-            class="play-btn__svg"
+            className="play-btn__svg"
             points="9.33 6.69 9.33 19.39 19.3 13.04 9.33 6.69"
           />
           <path
-            class="play-btn__svg"
+            className="play-btn__svg"
+            d="M26,13A13,13,0,1,1,13,0,13,13,0,0,1,26,13ZM13,2.18A10.89,10.89,0,1,0,23.84,13.06,10.89,10.89,0,0,0,13,2.18Z"
+          />
+        </svg>
+      </div>
+      <div className="musicPlayerPauseButtonhide" onClick={pauseAudio}>
+        <audio id="audio" src={soundFile} />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 26">
+          <polygon
+            className="play-btn__svg"
+            points="9.33 6.69 9.33 19.39 19.3 13.04 9.33 6.69"
+          />
+          <path
+            className="play-btn__svg"
             d="M26,13A13,13,0,1,1,13,0,13,13,0,0,1,26,13ZM13,2.18A10.89,10.89,0,1,0,23.84,13.06,10.89,10.89,0,0,0,13,2.18Z"
           />
         </svg>
