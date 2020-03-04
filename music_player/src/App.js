6379967f691c9430bd894aa68520c0dc318470d8
Jordi Suijkerbuijk <j.suijkerbuijk@hotmail.com>
2020-03-04 19:17:26 +0100

    Slidertest
    added new component to test the slider

--- music_player/src/App.js
+++ music_player/src/App.js
@@ -1,13 +1,15 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 //imports
-import MusicPlayer from "./components/musicPlayer/MusicPlayer";
+import MusicPlayer from './components/musicPlayer/MusicPlayer';
+import SliderTest from './components/sliderContainer/SliderContainer';
 
 function App() {
   return (
     <div className="App">
       <MusicPlayer />
+      <SliderTest />
     </div>
   );
 }
---
+++ music_player/src/components/sliderContainer/SliderContainer.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+import './sliderContainer.scss';
+
+function SliderContainer({}) {
+  return (
+    <div className="sliderContainer">
+      <div className="box" />
+    </div>
+  );
+}
+
+export default SliderContainer;
