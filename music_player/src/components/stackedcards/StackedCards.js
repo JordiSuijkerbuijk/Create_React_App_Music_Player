d384e71e4946cde1291e6f5777b051b47bbae7f9
Jordi Suijkerbuijk <j.suijkerbuijk@hotmail.com>
2020-06-16 11:15:00 +0200

    Update StackedCards.js

--- music_player/src/components/stackedcards/StackedCards.js
+++ music_player/src/components/stackedcards/StackedCards.js
@@ -9,7 +9,24 @@ function StackedCards() {
   function myFunction(e, wrapper, body, sectionWrapper, scale) {
     const clientHeight = wrapper.children[0].clientHeight;
     let deltaY = 0;
-      
+
+    if (
+      window.pageYOffset > wrapper.offsetParent.offsetTop - 100 &&
+      window.pageYOffset < wrapper.offsetParent.offsetTop
+    ) {
+      if (position > clientHeight * (wrapper.children.length - 1)) {
+        body.style.overflow = 'auto';
+      } else if (position <= 0) {
+        body.style.overflow = 'auto';
+      } else if (
+        window.pageYOffset > wrapper.offsetParent.offsetTop - 100 &&
+        window.pageYOffset < wrapper.offsetParent.offsetTop
+      ) {
+        body.style.overflow = 'hidden';
+      } else {
+        body.style.overflow = 'hidden';
+      }
+
       // determine scrollspeed, so that scrolling won't be to fast;
       if (e.deltaY > 0) {
         deltaY = e.deltaY > 3 ? 3 : e.deltaY;
@@ -51,7 +68,26 @@ function StackedCards() {
         }
       }
 
-    if (window.pageYOffset < wrapper.offsetParent.offsetTop - 250) {
+      // if (i === 0) {
+      //   if (position > (i + 1) * clientHeight - 5) {
+      //     if (
+      //       position <
+      //       clientHeight - 5 * (wrapper.children.length - 1) + wrapper.children.length * 20
+      //     ) {
+      //       scale[i] = e.deltaY < 0 ? scale[i] + 0.0002 : scale[i] - 0.0002;
+      //       wrapper.children[i].style.transform = `scale(${scale[i] > 1 ? 1 : scale[i]})`;
+      //     }
+      //   } else if (
+      //     e.pageY < wrapper.offsetParent.offsetTop + 100 + wrapper.children.length * 20 ||
+      //     position ===
+      //       (wrapper.offsetParent.offsetTop - 5) * (wrapper.children.length - 1) +
+      //         wrapper.children.length * 20
+      //   ) {
+      //     wrapper.children[i].style.transform = `scale(1)`;
+      //     scale[i] = 1;
+      //   }
+      // }
+    } else if (window.pageYOffset < wrapper.offsetParent.offsetTop - 100) {
       for (var i2 = 0; i < wrapper.children.length; i2++) {
         wrapper.children[i].style.transform = `translateY(0px)`;
         // sectionWrapper.style.height = `${clientHeight - 5 + wrapper.children.length * 20}px`;
@@ -61,31 +97,36 @@ function StackedCards() {
 
   function scrolledFunction(wrapper) {
     const clientHeight = wrapper.children[0].clientHeight;
-    if (position >= 0 && window.pageYOffset >= (wrapper.offsetParent.offsetTop - 250) && window.pageYOffset < (clientHeight * (wrapper.children.length - 1))){
-      if (wrapper.children[wrapper.children.length - 1].style.transform === "translateY(-1365px)") {
+    if (
+      position >= 0 &&
+      window.pageYOffset >= wrapper.offsetParent.offsetTop - 250 &&
+      window.pageYOffset < clientHeight * (wrapper.children.length - 1)
+    ) {
+      if (wrapper.children[wrapper.children.length - 1].style.transform === 'translateY(-1365px)') {
         return false;
       }
       return true;
-    }
-    else if (position < 0) {
+    } else if (position < 0) {
       return false;
     }
   }
 
-  function testFunction (e, wrapper, body, sectionWrapper, scale ){
+  function testFunction(e, wrapper, body, sectionWrapper, scale) {
     const clientHeight = wrapper.children[0].clientHeight;
 
     if (scrolledFunction(wrapper) === true) {
-      if(window.pageYOffset >= (wrapper.offsetParent.offsetTop - 250) && window.pageYOffset < (clientHeight * (wrapper.children.length - 1))) {
-        if(window.scrollY > wrapper.offsetParent.offsetTop - 250) {
+      if (
+        window.pageYOffset >= wrapper.offsetParent.offsetTop - 250 &&
+        window.pageYOffset < clientHeight * (wrapper.children.length - 1)
+      ) {
+        if (window.scrollY > wrapper.offsetParent.offsetTop - 250) {
           window.scrollTo(0, wrapper.offsetParent.offsetTop - 250);
         }
 
         body.style.overflow = 'hidden';
-        myFunction(e, wrapper, body, sectionWrapper, scale)
+        myFunction(e, wrapper, body, sectionWrapper, scale);
       }
-    }
-    else {
+    } else {
       body.style.overflow = 'auto';
     }
   }
@@ -104,19 +145,15 @@ function StackedCards() {
     console.log(wrapper.offsetParent.offsetTop - 250);
 
     if (eventListener !== true) {
-      // 
+      //
       body.addEventListener('wheel', (e) => testFunction(e, wrapper, body, sectionWrapper, scale));
 
       if (window.pageYOffset > clientHeight * (wrapper.children.length - 1)) {
         position = clientHeight * (wrapper.children.length - 1);
 
         for (var i = 0; i < wrapper.children.length; i++) {
-          wrapper.children[i].style.transform = `translateY(-${
-            clientHeight * i
-          }px)`;
-          sectionWrapper.style.height = `${
-            clientHeight - 5 + wrapper.children.length * 20
-          }px`;
+          wrapper.children[i].style.transform = `translateY(-${clientHeight * i}px)`;
+          sectionWrapper.style.height = `${clientHeight - 5 + wrapper.children.length * 20}px`;
         }
       }
 
